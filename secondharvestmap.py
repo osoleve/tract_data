d11700f289c56e92a2cf1862f80736981a7aba9b
osoleve <andrew.levenson@gmail.com>
2025-02-26 20:21:43 -0500

    Update sidebar layout for usability

--- secondharvestmap.py
+++ secondharvestmap.py
@@ -84,12 +84,17 @@ with info.popover("", icon=":material/question_mark:", use_container_width=True)
 title.title("Census Tract Analysis")
 
 with st.sidebar:
-    tabs = st.tabs(
-        [":material/discover_tune: Control", ":material/home_pin: Addresses"]
-    )
-
-    st.session_state["address_tab"] = tabs[1]
-    with tabs[1]:
+    with st.expander("Address Overlay", icon=":material/home:"):
+        st.header("SHNWNC Programs")
+        show_programs = st.checkbox(
+        "Overlay Program Locations",
+        value=config.get("show_programs", False),
+        help="Toggle visibility of program markers on the map",
+        key="show_programs"
+        )
+        st.caption("Visualize the locations of SHNWNC programs on the map.")
+       
+        st.header("Upload Addresses")
         st.markdown(
             "Upload a file containing addresses or coordinates to overlay them on the map."
         )
@@ -105,199 +110,188 @@ with st.sidebar:
             df = pd.DataFrame()
             programs = set()
 
-        with st.expander(":material/palette: Address Display Settings"):
-            marker_config = config["sliders"]["marker_size"]
-            updated_marker_opacity = st.slider(
-                "Marker Opacity",
-                0.1,
-                1.0,
-                config["client_marker"]["opacity"],
-                step=0.05,
-                key="mop",
+        
+
+        if not df.empty:
+            # Download df as two files, one with rows with lat/lon and one with ones we couldn't geocode
+            lat_lon_df = df.dropna(subset=["lat", "lon"])
+            no_geo_df = df[df["lat"].isnull() | df["lon"].isnull()]
+            lat_lon_csv = lat_lon_df.to_csv(index=False)
+            no_geo_csv = no_geo_df[
+                [c for c in no_geo_df.columns if c not in ["lat", "lon"]]
+            ].to_csv(index=False)
+            with st.popover("Export", icon=":material/download:"):
+                button_cols = st.columns(2)
+                with button_cols[0]:
+                    st.download_button(
+                        label="Mapped Addresses",
+                        data=lat_lon_csv,
+                        file_name="geocoded_addresses.csv",
+                        mime="text/csv",
+                    )
+                with button_cols[1]:
+                    st.download_button(
+                        label="Failed Addresses",
+                        data=no_geo_csv,
+                        file_name="addresses_geocoder_failed_on.csv",
+                        mime="text/csv",
+                        disabled=no_geo_df.empty,
+                    )
+    with st.expander("Calculation Controls", expanded=True, icon=":material/tune:"):
+        slider_config = config["sliders"]["weight"]
+
+        sliders = st.container()
+        st.session_state["config"]["normalize"] = sliders.checkbox(
+            "Normalize Variables",
+            value=config.get("normalize", True),
+            help="Turn this off to use raw scores opposed to relative scores.",
+        )
+        sliders.write("### Factor Weights")
+        sliders.caption("Adjust how each factor influences the combined score.")
+        food_weight = sliders.slider(
+            "Food Insecurity Weight",
+            slider_config["min"],
+            slider_config["max"],
+            config.get("food_weight", 1.0),
+            step=slider_config["step"],
+            key="fw",
+            help="The weight of food insecurity in the calculation.",
+        )
+        poverty_weight = sliders.slider(
+            "Poverty Weight",
+            slider_config["min"],
+            slider_config["max"],
+            config.get("poverty_weight", 1.0),
+            step=slider_config["step"],
+            key="pw",
+            help="The weight of poverty in the calculation.",
+        )
+        vehicle_weight = sliders.slider(
+            "Vehicle Access Weight",
+            slider_config["min"],
+            slider_config["max"],
+            config.get("vehicle_weight", 0.33),
+            step=slider_config["step"],
+            key="vw",
+            help="The weight of not having a vehicle in the calculation.",
+        )
+        vehicle_num_toggle = st.checkbox(
+            "Include Households with Fewer Vehicles than Members",
+            key="vnt",
+            value=config.get("vehicle_num_toggle", False),
+        )
+
+    with st.expander("Display Controls", icon=":material/palette:"):
+        scale_config = config["sliders"]["scale_max"]
+        st.write("### Map Color Scale")
+        if config["scale_max"] == "auto":
+            default_scale_max = float(
+                st.session_state["tracts"].combined_pct.quantile(0.90)
             )
-            updated_marker_size = st.slider(
-                "Marker Size",
-                marker_config["min"],
-                marker_config["max"],
-                config["client_marker"]["size"],
-                step=marker_config["step"],
-            )
-            updated_marker_color = st.color_picker(
-                "Marker Color", fixed_client_marker_color
+        else:
+            default_scale_max = (
+                float(config["scale_max"])
+                if isinstance(config["scale_max"], (int, float))
+                else float(config["scale_max"][0])
             )
 
-            if not df.empty:
-                # Download df as two files, one with rows with lat/lon and one with ones we couldn't geocode
-                lat_lon_df = df.dropna(subset=["lat", "lon"])
-                no_geo_df = df[df["lat"].isnull() | df["lon"].isnull()]
-                lat_lon_csv = lat_lon_df.to_csv(index=False)
-                no_geo_csv = no_geo_df[
-                    [c for c in no_geo_df.columns if c not in ["lat", "lon"]]
-                ].to_csv(index=False)
-                with st.popover("Export", icon=":material/download:"):
-                    button_cols = st.columns(2)
-                    with button_cols[0]:
-                        st.download_button(
-                            label="Mapped Addresses",
-                            data=lat_lon_csv,
-                            file_name="geocoded_addresses.csv",
-                            mime="text/csv",
-                        )
-                    with button_cols[1]:
-                        st.download_button(
-                            label="Failed Addresses",
-                            data=no_geo_csv,
-                            file_name="addresses_geocoder_failed_on.csv",
-                            mime="text/csv",
-                            disabled=no_geo_df.empty,
-                        )
+        scale_max = st.slider(
+            "Color Scale Max",
+            min_value=float(scale_config["min"]),
+            max_value=float(scale_config["max"]),
+            value=default_scale_max,
+            step=float(scale_config["step"]),
+            key="sm",
+            help="Adjust to make differences between areas more visible",
+        )
+        opacity_config = config["sliders"]["map_opacity"]
+        map_opacity = st.slider(
+            "Map Opacity",
+            opacity_config["min"],
+            opacity_config["max"],
+            config["map_opacity"],
+            step=opacity_config["step"],
+            key="mo",
+        )
 
-    with tabs[0]:
-        with st.expander("Program Overlay", icon=":material/home:"):
-            show_programs = st.checkbox(
-            "Overlay Program Locations",
-            value=config.get("show_programs", False),
-            help="Toggle visibility of program markers on the map",
-            key="show_programs"
-            )
-            st.caption("Visualize the locations of SHNWNC programs on the map.")
-            st.header("Settings")
-            program_marker_opacity = st.slider(
-                "Program Marker Opacity",
-                0.1,
-                1.0,
-                config["program_marker"]["opacity"],
-                step=0.05,
-                key="pmo",
-            )
-            program_marker_size = st.slider(
-            "Program Marker Size",
-            config["sliders"]["marker_size"]["min"],
-            config["sliders"]["marker_size"]["max"],
-            config["program_marker"]["size"],
-            step=1,
-            key="pms",
-            )
-        with st.expander("Calculation Controls", expanded=True, icon=":material/tune:"):
-            slider_config = config["sliders"]["weight"]
+        st.write("### Client Markers")
+        marker_config = config["sliders"]["marker_size"]
+        updated_marker_opacity = st.slider(
+            "Marker Opacity",
+            0.1,
+            1.0,
+            config["client_marker"]["opacity"],
+            step=0.05,
+            key="mop",
+        )
+        updated_marker_size = st.slider(
+            "Marker Size",
+            marker_config["min"],
+            marker_config["max"],
+            config["client_marker"]["size"],
+            step=marker_config["step"],
+        )
+        updated_marker_color = st.color_picker(
+            "Marker Color", fixed_client_marker_color
+        )
 
-            sliders = st.container()
-            st.session_state["config"]["normalize"] = sliders.checkbox(
-                "Normalize Variables",
-                value=config.get("normalize", True),
-                help="Turn this off to use raw scores opposed to relative scores.",
-            )
-            sliders.write("#### Factor Weights")
-            sliders.caption("Adjust how each factor influences the combined score.")
-            food_weight = sliders.slider(
-                "Food Insecurity Weight",
-                slider_config["min"],
-                slider_config["max"],
-                config.get("food_weight", 1.0),
-                step=slider_config["step"],
-                key="fw",
-                help="The weight of food insecurity in the calculation.",
-            )
-            poverty_weight = sliders.slider(
-                "Poverty Weight",
-                slider_config["min"],
-                slider_config["max"],
-                config.get("poverty_weight", 1.0),
-                step=slider_config["step"],
-                key="pw",
-                help="The weight of poverty in the calculation.",
-            )
-            vehicle_weight = sliders.slider(
-                "Vehicle Access Weight",
-                slider_config["min"],
-                slider_config["max"],
-                config.get("vehicle_weight", 0.33),
-                step=slider_config["step"],
-                key="vw",
-                help="The weight of not having a vehicle in the calculation.",
-            )
-            vehicle_num_toggle = st.checkbox(
-                "Include Households with Fewer Vehicles than Members",
-                key="vnt",
-                value=config.get("vehicle_num_toggle", False),
-            )
-
-        if "tracts" not in st.session_state:
-            st.session_state["tracts"] = load_and_process_data(config)
-        config = st.session_state["config"]
+        st.write("### Program Markers")
+        st.caption("Adjust the appearance of program markers on the map.")
+        program_marker_opacity = st.slider(
+            "Program Marker Opacity",
+            0.1,
+            1.0,
+            config["program_marker"]["opacity"],
+            step=0.05,
+            key="pmo",
+        )
+        program_marker_size = st.slider(
+        "Program Marker Size",
+        config["sliders"]["marker_size"]["min"],
+        config["sliders"]["marker_size"]["max"],
+        config["program_marker"]["size"],
+        step=1,
+        key="pms",
+        )
+        
         config = update_config(
             config,
-            client_marker={
-                "size": updated_marker_size,
-                "color": updated_marker_color,
-                "opacity": updated_marker_opacity,
+            scale_max=scale_max,
+            map_opacity=map_opacity,
+            show_programs=show_programs,
+            program_marker={
+                "opacity": program_marker_opacity,
+                "size": program_marker_size,
             },
-            poverty_weight=poverty_weight,
-            food_weight=food_weight,
-            vehicle_weight=vehicle_weight,
-            vehicle_num_toggle=vehicle_num_toggle,
-            show_settings=False,
-            map_type=st.session_state["map_type"],
         )
         st.session_state["config"] = config
+if "tracts" not in st.session_state:
+    st.session_state["tracts"] = load_and_process_data(config)
 
-        st.session_state["tracts"] = post_process_data(
-            st.session_state["tracts"],
-            vehicle_num_toggle,
-            poverty_weight,
-            vehicle_weight,
-            food_weight,
-        )
+config = st.session_state["config"]
+config = update_config(
+    config,
+    client_marker={
+        "size": updated_marker_size,
+        "color": updated_marker_color,
+        "opacity": updated_marker_opacity,
+    },
+    poverty_weight=poverty_weight,
+    food_weight=food_weight,
+    vehicle_weight=vehicle_weight,
+    vehicle_num_toggle=vehicle_num_toggle,
+    show_settings=False,
+    map_type=st.session_state["map_type"],
+)
+st.session_state["config"] = config
 
-        with st.expander("Color", icon=":material/palette:"):
-            scale_config = config["sliders"]["scale_max"]
-            cols = st.columns(2)
-            # Convert default value to float and handle various input types
-            if config["scale_max"] == "auto":
-                default_scale_max = float(
-                    st.session_state["tracts"].combined_pct.quantile(0.90)
-                )
-            else:
-                default_scale_max = (
-                    float(config["scale_max"])
-                    if isinstance(config["scale_max"], (int, float))
-                    else float(config["scale_max"][0])
-                )
-
-            scale_max = st.slider(
-                "Color Scale Max",
-                min_value=float(scale_config["min"]),
-                max_value=float(scale_config["max"]),
-                value=default_scale_max,
-                step=float(scale_config["step"]),
-                key="sm",
-                help="Adjust to make differences between areas more visible",
-            )
-            opacity_config = config["sliders"]["map_opacity"]
-            map_opacity = st.slider(
-                "Map Opacity",
-                opacity_config["min"],
-                opacity_config["max"],
-                config["map_opacity"],
-                step=opacity_config["step"],
-                key="mo",
-            )
-            
-
-    
-
-            config = update_config(
-                config,
-                scale_max=scale_max,
-                map_opacity=map_opacity,
-                show_programs=show_programs,
-                program_marker={
-                    "opacity": program_marker_opacity,
-                    "size": program_marker_size,
-                },
-            )
-            st.session_state["config"] = config
-
+st.session_state["tracts"] = post_process_data(
+    st.session_state["tracts"],
+    vehicle_num_toggle,
+    poverty_weight,
+    vehicle_weight,
+    food_weight,
+)
 try:
     fig = make_map(st.session_state["tracts"], "combined_pct", config)
     st.plotly_chart(fig, use_container_width=True)
